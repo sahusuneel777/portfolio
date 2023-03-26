8828da07f0db9a2971c1a3871e8744ccb1663140
suneel <tarun.yajjala@gmail.com>
2023-03-26 15:15:18 +0530

    expertise added

---
+++ src/components/Expertise/Expertise.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import css from './Expertise.module.scss'
+import { projectExperience, WhatDoIHelp } from '../../utils/data'
+import { motion } from "framer-motion"
+import { fadeIn, staggerContainer, textVariant } from '../../utils/motion'
+
+
+const Expertise = () => {
+    return (
+        <section className={`${css.wrapper}`}>
+            <motion.div 
+            variants={staggerContainer}
+            whileInView="show"
+            initial="hidden"
+            viewport={{ once: false, amount: 0.25 }}
+             className={`flexCenter yPaddings innerWidth ${css.container}`}>
+                <div className={`${css.leftside}`}>
+                    {projectExperience.map((exp, i) => {
+                        return (<motion.div
+                        variants={fadeIn("right","tween",(i+1)*0.2,1)}
+                         key={i} className={`${css.exp}`}>
+                            <exp.icon size={15} color="white" style={{ backgroundColor: exp.bg }} />
+                            <div className={`${css.expDetails}`}>
+                                <span>{exp.name}</span>
+                                <span className='secondaryText'>{`${exp.projects} projects`}</span>
+                            </div>
+                        </motion.div>)
+                    })
+                    }
+                </div>
+
+                <motion.div 
+                variants={textVariant(0.5)}
+                className={`${css.rightside}`}>
+                    <span className='primaryText'>What do i do?</span>
+                    {WhatDoIHelp.map((help, i) => {
+                        return (
+                            <div className='secondaryText' key={i}>
+                                {help}
+                            </div>
+                        )
+                    })}
+                </motion.div>
+            </motion.div>
+        </section>
+    )
+}
+
+export default Expertise
